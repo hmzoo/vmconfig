2cfbd769a505b7a29a508bf204fc4b46e6f047bd
HMJ <hmj@goatpage.com>
2026-05-07 10:27:42 +0200

    Add Python base tooling packages

--- install_base.sh
+++ install_base.sh
@@ -50,6 +50,10 @@ ${SUDO} apt-get install -y \
 	apt-transport-https \
 	software-properties-common \
 	git \
+	python3 \
+	python3-pip \
+	python3-venv \
+	python-is-python3 \
 	jq \
 	unzip \
 	zip \
